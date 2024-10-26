633dfcd04d156e294f2f6698b5d5e8abf77fc345
Wccccc <43540548+cjim8889@users.noreply.github.com>
2024-10-26 22:32:32 +0100

    some change

--- liouville_job.sh
+++ liouville_job.sh
@@ -16,3 +16,5 @@ source activate py12
 python train_modified_liouville.py --N 1024 --T 128 --schedule inverse_power --mcmc_type hmc --num_mcmc_steps 5 --num_mcmc_integration_steps 5 --eta 1.0 --num_epochs 8000 --num_steps 50
 # Deactivate the virtual environment
 deactivate
+
+uv run train_modified_liouville.py --N 1024 --T 128 --schedule inverse_power --mcmc_type hmc --num_mcmc_steps 5 --num_mcmc_integration_steps 5 --eta 1.0 --num_epochs 8000 --num_steps 50
