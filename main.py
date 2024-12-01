aba542b2e88d6a7081445bdd6104c77b884d8d61
J-zin <670163670@qq.com>
2024-12-01 00:28:06 +0000

    working on dw4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from experiments.gmm.train_liouville import main as train_liouville_gmm
 from experiments.many_well.train_liouville import main as train_liouville_many_well
+from experiments.double_well.train_liouville import main as train_liouville_double_well
 
 def execute_function(method, exp):
     main_fn = eval(f'train_{method}_{exp}')
@@ -9,5 +10,6 @@ def execute_function(method, exp):
 
 if __name__ == '__main__':
     # main_fn = execute_function('liouville', 'gmm')
-    main_fn = execute_function('liouville', 'many_well')
+    # main_fn = execute_function('liouville', 'many_well')
+    main_fn = execute_function('liouville', 'double_well')
     main_fn()
