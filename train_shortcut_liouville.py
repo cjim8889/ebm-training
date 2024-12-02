814169f23dcaa460e9eb073d4eb3613da56e6893
Wccccc <43540548+cjim8889@users.noreply.github.com>
2024-12-02 11:58:46 +0800

    Add MW32

--- train_shortcut_liouville.py
+++ train_shortcut_liouville.py
@@ -86,6 +86,261 @@ def plot_marginal_pair(
     )
 
 
+def rejection_sampling(
+    n_samples: int,
+    proposal: distrax.Distribution,
+    target_log_prob_fn: Callable,
+    k: float,
+    key: chex.PRNGKey,
+) -> chex.Array:
+    """Rejection sampling. See Pattern Recognition and ML by Bishop Chapter 11.1"""
+    # Note: This currently is not written to work inside of jax.jit or jax.vmap.
+    key1, key2, key3 = jax.random.split(key, 3)
+    n_samples_propose = n_samples * 10
+    z_0, log_q_z0 = proposal._sample_n_and_log_prob(key, n=n_samples_propose)
+    u_0 = (
+        jax.random.uniform(key=key2, shape=(n_samples_propose,)) * k * jnp.exp(log_q_z0)
+    )
+    accept = jnp.exp(target_log_prob_fn(z_0)) > u_0
+    samples = z_0[accept]
+    if samples.shape[0] >= n_samples:
+        return samples[:n_samples]
+    else:
+        required_samples = n_samples - samples.shape[0]
+        new_samples = rejection_sampling(
+            required_samples, proposal, target_log_prob_fn, k, key3
+        )
+        samples = jnp.concatenate([samples, new_samples], axis=0)
+        return samples
+
+
+class Energy:
+    """
+    https://zenodo.org/record/3242635#.YNna8uhKjIW
+    """
+
+    def __init__(self, dim):
+        super().__init__()
+        self._dim = dim
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def _energy(self, x):
+        raise NotImplementedError()
+
+    def energy(self, x, temperature=None):
+        assert x.shape[-1] == self._dim, "`x` does not match `dim`"
+        if temperature is None:
+            temperature = 1.0
+        return self._energy(x) / temperature
+
+    def force(self, x, temperature=None):
+        e_func = lambda x: jnp.sum(self.energy(x, temperature=temperature))
+        return -jax.grad(e_func)(x)
+
+
+class DoubleWellEnergy(Energy):
+    def __init__(self):
+        dim = 2
+        a = -0.5
+        b = -6.0
+        c = 1.0
+        super().__init__(dim)
+        self._a = a
+        self._b = b
+        self._c = c
+
+    def _energy(self, x):
+        d = x[:, [0]]
+        v = x[:, 1:]
+        e1 = self._a * d + self._b * d**2 + self._c * d**4
+        e2 = jnp.sum(0.5 * v**2, axis=-1, keepdims=True)
+        return e1 + e2
+
+    def log_prob(self, x):
+        if len(x.shape) == 1:
+            x = jnp.expand_dims(x, axis=0)
+        return jnp.squeeze(-self.energy(x))
+
+    @property
+    def log_Z(self):
+        log_Z_dim0 = jnp.log(11784.50927)
+        log_Z_dim1 = 0.5 * jnp.log(2 * jnp.pi)
+        return log_Z_dim0 + log_Z_dim1
+
+    def sample_first_dimension(self, key: chex.Array, n: int) -> chex.Array:
+        # see fab.sampling.rejection_sampling_test.py
+        if self._a == -0.5 and self._b == -6 and self._c == 1.0:
+            # Define target.
+            def target_log_prob(x):
+                return -(x**4) + 6 * x**2 + 1 / 2 * x
+
+            TARGET_Z = 11784.50927
+
+            # Define proposal params
+            component_mix = jnp.array([0.2, 0.8])
+            means = jnp.array([-1.7, 1.7])
+            scales = jnp.array([0.5, 0.5])
+
+            # Define proposal
+            mix = distrax.Categorical(component_mix)
+            com = distrax.Normal(means, scales)
+
+            proposal = distrax.MixtureSameFamily(
+                mixture_distribution=mix, components_distribution=com
+            )
+
+            k = TARGET_Z * 3
+
+            samples = rejection_sampling(
+                n_samples=n,
+                proposal=proposal,
+                target_log_prob_fn=target_log_prob,
+                k=k,
+                key=key,
+            )
+            return samples
+        else:
+            raise NotImplementedError
+
+    def sample(self, key: chex.PRNGKey, shape: chex.Shape):
+        if self._a == -0.5 and self._b == -6 and self._c == 1.0:
+            assert len(shape) == 1
+            key1, key2 = jax.random.split(key=key)
+            dim1_samples = self.sample_first_dimension(key=key1, n=shape[0])
+            dim2_samples = distrax.Normal(jnp.array(0.0), jnp.array(1.0)).sample(
+                seed=key2, sample_shape=shape
+            )
+            return jnp.stack([dim1_samples, dim2_samples], axis=-1)
+        else:
+            raise NotImplementedError
+
+
+class ManyWellEnergy(Target):
+    def __init__(self, dim: int = 32):
+        assert dim % 2 == 0
+        self.n_wells = dim // 2
+        self.double_well_energy = DoubleWellEnergy()
+
+        log_Z = self.double_well_energy.log_Z * self.n_wells
+        super().__init__(
+            dim=dim,
+            log_Z=log_Z,
+            can_sample=False,
+            n_plots=1,
+            n_model_samples_eval=2000,
+            n_target_samples_eval=10000,
+        )
+
+        self.centre = 1.7
+        self.max_dim_for_all_modes = (
+            40  # otherwise we get memory issues on huuuuge test set
+        )
+        if self.dim < self.max_dim_for_all_modes:
+            dim_1_vals_grid = jnp.meshgrid(
+                *[jnp.array([-self.centre, self.centre]) for _ in range(self.n_wells)]
+            )
+            dim_1_vals = jnp.stack([dim.flatten() for dim in dim_1_vals_grid], axis=-1)
+            n_modes = 2**self.n_wells
+            assert n_modes == dim_1_vals.shape[0]
+            test_set = jnp.zeros((n_modes, dim))
+            test_set = test_set.at[:, jnp.arange(dim) % 2 == 0].set(dim_1_vals)
+            self.modes_test_set = test_set
+        else:
+            raise NotImplementedError("still need to implement this")
+
+        self.shallow_well_bounds = [-1.75, -1.65]
+        self.deep_well_bounds = [1.7, 1.8]
+        self._plot_bound = 3.0
+
+        self.double_well_samples = self.double_well_energy.sample(
+            key=jax.random.PRNGKey(0), shape=(int(1e6),)
+        )
+
+        if self.n_target_samples_eval < self.modes_test_set.shape[0]:
+            print("Evaluation occuring on subset of the modes test set.")
+
+    def log_prob(self, x):
+        return jnp.sum(
+            jnp.stack(
+                [
+                    self.double_well_energy.log_prob(x[..., i * 2 : i * 2 + 2])
+                    for i in range(self.n_wells)
+                ],
+                axis=-1,
+            ),
+            axis=-1,
+        )
+
+    def log_prob_2D(self, x):
+        """Marginal 2D pdf - useful for plotting."""
+        return self.double_well_energy.log_prob(x)
+
+    def get_target_log_prob_marginal_pair(self, i: int, j: int, total_dim: int):
+        def log_prob_marginal_pair(x_2d):
+            x = jnp.zeros((x_2d.shape[0], total_dim))
+            x = x.at[:, i].set(x_2d[:, 0])
+            x = x.at[:, j].set(x_2d[:, 1])
+            return self.log_prob(x)
+
+        return log_prob_marginal_pair
+
+    def visualise(self, samples: chex.Array) -> plt.Figure:
+        alpha = 0.3
+        plotting_bounds = (-3, 3)
+        dim = samples.shape[-1]
+        fig, axs = plt.subplots(2, 2, sharex="row", sharey="row", figsize=(10, 8))
+
+        for i in range(2):
+            for j in range(2):
+                target_log_prob = self.get_target_log_prob_marginal_pair(i, j + 2, dim)
+                plot_contours_2D(
+                    target_log_prob, bound=self._plot_bound, ax=axs[i, j], levels=20
+                )
+                plot_marginal_pair(
+                    samples,
+                    marginal_dims=(i, j + 2),
+                    ax=axs[i, j],
+                    bounds=plotting_bounds,
+                    alpha=alpha,
+                )
+
+                if j == 0:
+                    axs[i, j].set_ylabel(f"$x_{{{i + 1}}}$")
+                if i == 1:
+                    axs[i, j].set_xlabel(f"$x_{{{j + 3}}}$")
+
+        plt.tight_layout()
+        return fig
+
+    def get_eval_samples(
+        self, key: chex.PRNGKey, n: int
+    ) -> Tuple[chex.Array, chex.Array]:
+        key1, key2 = jax.random.split(key)
+        dw_sample_indices = jax.random.randint(
+            minval=0,
+            maxval=self.double_well_samples.shape[0],
+            key=key1,
+            shape=(n * self.n_wells,),
+        )
+        dw_samples = self.double_well_samples[dw_sample_indices]
+        samples_p = jnp.reshape(dw_samples, (-1, self.dim))
+
+        if n < self.modes_test_set.shape[0]:
+            mode_sample_indices = jax.random.choice(
+                a=jnp.arange(self.modes_test_set.shape[0]),
+                key=key2,
+                shape=(n,),
+                replace=False,
+            )
+            samples_modes = self.modes_test_set[mode_sample_indices]
+        else:
+            samples_modes = self.modes_test_set
+        return samples_p, samples_modes
+
+
 class MultivariateGaussian(Target):
     def __init__(
         self, dim: int = 2, sigma: float = 1.0, plot_bound_factor: float = 3.0
@@ -939,6 +1194,7 @@ def main():
     parser.add_argument("--eta", type=float, default=0.2)
     parser.add_argument("--initial-sigma", type=float, default=20.0)
     parser.add_argument("--eval-steps", type=int, nargs="+", default=[4, 8, 16, 32])
+    parser.add_argument("--target", type=str, default="gmm", choices=["gmm", "mw32"])
     parser.add_argument(
         "--d-distribution", type=str, choices=["uniform", "log"], default="uniform"
     )
@@ -961,16 +1217,27 @@ def main():
     parser.add_argument("--with-rejection-sampling", action="store_true")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--offline", action="store_true")
+    parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
+
+    if args.debug:
+        jax.config.update("jax_debug_nans", True)
 
     # Set random seed
     key = jax.random.PRNGKey(args.seed)
 
-    input_dim = 2
-    key, subkey = jax.random.split(key)
-    # Initialize distributions
-    initial_density = MultivariateGaussian(dim=input_dim, sigma=args.initial_sigma)
-    target_density = GMM(subkey, dim=input_dim)
+    # Set up distributions
+    if args.target == "gmm":
+        input_dim = 2
+        key, subkey = jax.random.split(key)
+        # Initialize distributions
+        initial_density = MultivariateGaussian(dim=input_dim, sigma=args.initial_sigma)
+        target_density = GMM(subkey, dim=input_dim)
+    elif args.target == "mw32":
+        input_dim = 32
+        key, subkey = jax.random.split(key)
+        initial_density = MultivariateGaussian(dim=input_dim, sigma=args.initial_sigma)
+        target_density = ManyWellEnergy(dim=input_dim)
 
     # Initialize velocity field
     key, model_key = jax.random.split(key)
@@ -1003,8 +1270,10 @@ def main():
                 "with_rejection_sampling": args.with_rejection_sampling,
                 "continuous_schedule": args.continuous_schedule,
                 "d_distribution": args.d_distribution,
+                "target": args.target,
             },
             reinit=True,
+            tags=[args.target],
         )
 
     # Train model
