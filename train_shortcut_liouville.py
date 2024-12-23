a94a74cb26c5b8badb80a03cde36378eede92c68
Wccccc <43540548+cjim8889@users.noreply.github.com>
2024-12-23 12:51:23 +0800

    Update alpha parameter in train_shortcut_liouville to enhance model stability

--- train_shortcut_liouville.py
+++ train_shortcut_liouville.py
@@ -3180,7 +3180,7 @@ def main():
             dim=input_dim,
             n_particles=13,
             sigma=1.0,
-            alpha=0.8,
+            alpha=0.1,
             epsilon_val=1.0,
             min_dr=1e-3,
             m=1,
