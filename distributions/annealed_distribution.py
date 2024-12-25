02aa8fef7702b4921536b64b24e2c5e77343b2d1
Wccccc <43540548+cjim8889@users.noreply.github.com>
2024-12-25 11:04:08 +0800

    Update .gitignore, refactor distributions, and add new experiments for SMC inference

--- distributions/annealed_distribution.py
+++ distributions/annealed_distribution.py
@@ -8,33 +8,30 @@ class AnnealedDistribution(Target):
 
     def __init__(
         self,
-        initial_distribution: Target,
-        target_distribution: Target,
-        dim: int = 2,
-        max_score: float = 1.0,
+        initial_density: Target,
+        target_density: Target,
     ):
         super().__init__(
-            dim=dim,
+            dim=initial_density.dim,
             log_Z=0.0,
             can_sample=False,
             n_plots=1,
             n_model_samples_eval=1000,
             n_target_samples_eval=1000,
         )
-        self.initial_distribution = initial_distribution
-        self.target_distribution = target_distribution
-        self.max_score = max_score
+        self.initial_density = initial_density
+        self.target_density = target_density
 
     def log_prob(self, xs: chex.Array) -> chex.Array:
         return self.time_dependent_log_prob(xs, 1.0)
 
     def time_dependent_log_prob(self, xs: chex.Array, t: chex.Array) -> chex.Array:
-        initial_prob = (1 - t) * self.initial_distribution.log_prob(xs)
+        initial_prob = (1 - t) * self.initial_density.log_prob(xs)
 
-        if self.target_distribution.TIME_DEPENDENT:
-            target_prob = t * self.target_distribution.time_dependent_log_prob(xs, t)
+        if self.target_density.TIME_DEPENDENT:
+            target_prob = t * self.target_density.time_dependent_log_prob(xs, t)
         else:
-            target_prob = t * self.target_distribution.log_prob(xs)
+            target_prob = t * self.target_density.log_prob(xs)
 
         return initial_prob + target_prob
 
@@ -45,4 +42,4 @@ class AnnealedDistribution(Target):
         return jax.grad(lambda x: self.time_dependent_log_prob(x, t))(xs)
 
     def sample_initial(self, key: chex.PRNGKey, sample_shape: chex.Shape) -> chex.Array:
-        return self.initial_distribution.sample(key, sample_shape)
+        return self.initial_density.sample(key, sample_shape)
