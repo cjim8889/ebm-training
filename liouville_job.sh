2cd026effb5d6238ec79aa9eb4987876e392bf36
Wccccc <43540548+cjim8889@users.noreply.github.com>
2024-12-25 15:52:53 +0800

    Update training scripts to enhance parameter configuration and switch to new main execution flow

--- liouville_job.sh
+++ liouville_job.sh
@@ -13,8 +13,26 @@ cd $PBS_O_WORKDIR
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate py12
 
-python train_modified_liouville.py --N 5120 --T 128 --schedule linear --optimiser sgd --mcmc_type hmc --num_mcmc_steps 5 --num_mcmc_integration_steps 3 --eta 1.0 --num_epochs 8000 --num_steps 50 --seed 923894
-# Deactivate the virtual environment
-deactivate
+python main.py \
+  --num-epochs=8000 \
+  --batch-size=256 \
+  --num-samples=5120 \
+  --num-steps=200 \
+  --learning-rate=1e-3 \
+  --hidden-dim=256 \
+  --depth=4 \
+  --num-timesteps=128 \
+  --mcmc-steps=5 \
+  --mcmc-integration-steps=10 \
+  --eta=0.05 \
+  --schedule="power" \
+  --integrator="euler" \
+  --optimizer="adamw" \
+  --target="lj13bt" \
+  --seed=97979 \
+  --continuous-schedule \
+  --soft-clip \
+  --target-end-time 1. \
+  --offline
 
 # CUDA_VISIBLE_DEVICES=3 uv run train_modified_liouville.py --hidden_dim 256 --N 1024 --T 128 --schedule inverse_power --input_dim 10 --depth 3 --mcmc_type langevin --num_mcmc_steps 5 --num_mcmc_integration_steps 5 --eta 1.0 --num_epochs 16000 --num_steps 50
