c14ef0f9db721479015b7777226e986597e02495
J-zin <670163670@qq.com>
2024-12-25 04:39:33 +0000

    working on gmm

---
+++ run.sh
@@ -0,0 +1,4 @@
+# CUDA_VISIBLE_DEVICES=0 python dem/train.py experiment=gmm_idem
+# CUDA_VISIBLE_DEVICES=0 python dem/train.py experiment=lj13_idem
+
+CUDA_VISIBLE_DEVICES=0 python dem/train.py experiment=gmm_liouville model=liouville
