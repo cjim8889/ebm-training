cbdc87199d772cdfd50e8bc727ce626384b35c1b
J-zin <670163670@qq.com>
2024-12-26 13:02:19 +0000

    add gmm

--- dem/models/liouville_module.py
+++ dem/models/liouville_module.py
@@ -1,5 +1,7 @@
 from einops import rearrange, repeat
 
+from torchmetrics import MeanMetric
+
 from .dem_module import *
 from .components.mcmc import time_batched_sample_hamiltonian_monte_carlo
 
@@ -99,13 +101,171 @@ class LiouvilleModule(DEMLitModule):
             num_negative_time_steps=num_negative_time_steps,
         )
 
-    def on_train_epoch_start(self):
-        pass
+        self.liouville_train_loss = MeanMetric()
+
+    def get_loss(self, ts, xs):
+        ts = ts.to(xs.device)
+        ts = repeat(ts, 't -> n t', n=xs.size(0))
+
+        dt_log_unormalised_density = self.time_derivative_log_density(xs, ts)
+        dt_log_density = dt_log_unormalised_density - dt_log_unormalised_density.mean(dim=0, keepdim=True)
+
+        score = self.score_function(xs, ts)
+
+        b, t, d = xs.shape
+        xs = rearrange(xs, "b t d -> (b t) d")
+        ts = rearrange(ts, "b t -> (b t)")
+
+        xs_detached = xs.detach().requires_grad_(True)
+        v = self.net(ts.unsqueeze(-1), self.energy_function.normalize(xs_detached))
+
+        div_v = torch.zeros(xs_detached.shape[:1], device=xs_detached.device)
+        for i in range(xs_detached.shape[-1]):
+            div_v += torch.autograd.grad(v[..., i].sum(), xs_detached, create_graph=True, retain_graph=True)[0][..., i]
+
+        div_v = rearrange(div_v, "(b t) -> b t", b=b)
+        v = rearrange(v, "(b t) d -> b t d", b=b)
+
+        lhs = div_v + (v * score).sum(dim=-1)
+        eps = (lhs + dt_log_density).nan_to_num_(posinf=1.0, neginf=-1.0, nan=0.0)
+        return (eps**2).mean()
 
     def training_step(self, batch, batch_idx):
         loss = 0.0
-        print(self.training_data.shape)
-        exit()
+
+        noised_samples = self.training_data[0][
+            torch.randperm(self.training_data[0].size(0))[: self.num_samples_to_sample_from_buffer]
+        ].detach()
+        times = self.training_data[1]
+        
+        liouville_loss = self.get_loss(times, noised_samples)
+        loss += liouville_loss
+
+        self.liouville_train_loss(liouville_loss)
+        self.log(
+            "train/liouville_train_loss",
+            self.liouville_train_loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+        return loss
+
+    def on_train_epoch_end(self) -> None:
+        "Lightning hook that is called when a training epoch ends."
+
+        self.training_data = self.generate_samples_with_hmc_corrector()
+
+        initial_samples = self.prior.sample(self.num_samples_to_generate_per_epoch)
+        ts = torch.linspace(0, 1, self.time_schedule.T)
+        self.last_samples = self.generate_samples(initial_samples, ts)[-1, ...].detach()
+        self.last_energies = self.energy_function(self.energy_function.normalize(self.last_samples))
+        
+        self._log_energy_w2(prefix="val")
+        if self.energy_function.is_molecule:
+            self._log_dist_w2(prefix="val")
+            self._log_dist_total_var(prefix="val")
+
+    def validation_step(self, batch: torch.Tensor, batch_idx: int) -> None:
+        self.eval_step("val", batch, batch_idx)
+
+    def test_step(self, batch: torch.Tensor, batch_idx: int) -> None:
+        self.eval_step("test", batch, batch_idx)
+
+    def eval_step(self, prefix: str, batch: torch.Tensor, batch_idx: int) -> None:
+        """Perform a single eval step on a batch of data from the validation set.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target
+            labels.
+        :param batch_idx: The index of the current batch.
+        """
+        return
+
+    def on_validation_epoch_end(self) -> None:
+        self.eval_epoch_end("val")
+
+    def on_test_epoch_end(self) -> None:
+        self.eval_epoch_end("test")
+
+    def eval_epoch_end(self, prefix: str):
+        wandb_logger = get_wandb_logger(self.loggers)
+
+        self.energy_function.log_on_epoch_end(
+            self.last_samples,
+            self.last_energies,
+            wandb_logger,
+        )
+
+    def _log_energy_w2(self, prefix="val"):
+        data_set = self.energy_function.sample_test_set(self.eval_batch_size)
+        generated_energies = self.last_energies[: data_set.size(0)]
+
+        energies = self.energy_function(self.energy_function.normalize(data_set))
+        energy_w2 = pot.emd2_1d(energies.cpu().numpy(), generated_energies.cpu().numpy())
+
+        self.log(
+            f"{prefix}/energy_w2",
+            self.val_energy_w2(energy_w2),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+    def _log_dist_w2(self, prefix="val"):
+        data_set = self.energy_function.sample_test_set(self.eval_batch_size)
+        generated_samples = self.last_samples[: data_set.size(0)]
+
+        dist_w2 = pot.emd2_1d(
+            self.energy_function.interatomic_dist(generated_samples).cpu().numpy().reshape(-1),
+            self.energy_function.interatomic_dist(data_set).cpu().numpy().reshape(-1),
+        )
+        self.log(
+            f"{prefix}/dist_w2",
+            self.val_dist_w2(dist_w2),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+    def _log_dist_total_var(self, prefix="val"):
+        data_set = self.energy_function.sample_test_set(self.eval_batch_size)
+        generated_samples = self.last_samples[: data_set.size(0)]
+
+        generated_samples_dists = (
+            self.energy_function.interatomic_dist(generated_samples).cpu().numpy().reshape(-1),
+        )
+        data_set_dists = self.energy_function.interatomic_dist(data_set).cpu().numpy().reshape(-1)
+
+        H_data_set, x_data_set = np.histogram(data_set_dists, bins=200)
+        H_generated_samples, _ = np.histogram(generated_samples_dists, bins=(x_data_set))
+        total_var = (
+            0.5
+            * np.abs(
+                H_data_set / H_data_set.sum() - H_generated_samples / H_generated_samples.sum()
+            ).sum()
+        )
+
+        self.log(
+            f"{prefix}/dist_total_var",
+            self.val_dist_total_var(total_var),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+    def score_function(self, x, t):
+        x = x.detach().requires_grad_(True)
+        log_p = self.time_dependent_log_density(x, t)
+        return torch.autograd.grad(log_p.sum(), x)[0]
+
+    def time_derivative_log_density(self, x, t=None):
+        x = self.energy_function.normalize(x)
+        return - self.prior.log_prob(x) + self.energy_function(x)
+
+    def time_dependent_log_density(self, x, t):
+        x = self.energy_function.normalize(x)
+        return (1 - t) * self.prior.log_prob(x) + t * self.energy_function(x)
 
     def generate_samples(self, initial_samples, ts):
         """
@@ -120,12 +280,11 @@ class LiouvilleModule(DEMLitModule):
         for t_p, t_n in zip(t_prev, t_next):
             t = torch.ones(samples.size(0), device=self.device).unsqueeze(1) * t_p
             with torch.no_grad():
-                samples = samples + self.net(t, samples) * (t_n - t_p)
+                samples = samples + self.net(t, self.energy_function.normalize(samples)) * (t_n - t_p)
             samples_list.append(samples)
     
         samples = torch.stack(samples_list, dim=0)
         return samples
-
 
     def generate_samples_with_hmc_corrector(self):
         initial_samples = self.prior.sample(self.num_samples_to_generate_per_epoch)
@@ -142,7 +301,7 @@ class LiouvilleModule(DEMLitModule):
             rejection_sampling=False,
         )
         final_samples = rearrange(final_samples, 't n d -> n t d')
-        return final_samples
+        return (final_samples, ts)
 
     def setup(self, stage: str) -> None:
         """Lightning hook that is called at the beginning of fit (train + validate), validate,
@@ -156,9 +315,6 @@ class LiouvilleModule(DEMLitModule):
         self.prior = self.partial_prior(device=self.device)
         self.time_schedule = self.hparams.time_schedule
         self.net = self.net.to(self.device)
-
-        # set up various functions
-        self.time_dependent_log_density = lambda x, t: (1 - t) * self.prior.log_prob(x) + t * self.energy_function(x)
 
         if stage == "fit":
             # generate training data
