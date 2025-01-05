36e81df1e05c9e0c46dd8b944644b96a51a4a9b1
wc5118 <43540548+cjim8889@users.noreply.github.com>
2025-01-05 07:32:01 +0000

    Refactor sampling script and SMC utilities for improved clarity and performance

--- experiments/sample_lj13c.py
+++ experiments/sample_lj13c.py
@@ -8,10 +8,9 @@ from distributions.time_dependent_lennard_jones_butler import (
     TimeDependentLennardJonesEnergyButler,
     TimeDependentLennardJonesEnergyButlerWithTemperatureTempered,
 )
-from distributions import SoftCoreLennardJonesEnergy
 
 from utils.optimization import power_schedule, inverse_power_schedule
-from utils.smc import generate_samples_with_smc, SampleBuffer
+from utils.smc import generate_samples_with_smc
 
 key = jax.random.PRNGKey(1234)
 
@@ -19,7 +18,7 @@ initial_density = MultivariateGaussian(dim=39, mean=0, sigma=3)
 target_density = TimeDependentLennardJonesEnergyButler(
     dim=39,
     n_particles=13,
-    alpha=0.5,
+    alpha=0.2,
     sigma=1.0,
     epsilon_val=1.0,
     min_dr=1e-4,
@@ -27,64 +26,12 @@ target_density = TimeDependentLennardJonesEnergyButler(
     m=1,
     c=0.5,
     include_harmonic=True,
-    # log_prob_clip=100.0,
 )
-
-# target_density = SoftCoreLennardJonesEnergy(
-#     dim=39,
-#     n_particles=13,
-#     sigma=1.0,
-#     epsilon_val=1.0,
-#     min_dr=1e-4,
-#     alpha=0.2,
-#     c=0.5,
-#     include_harmonic=True,
-#     log_prob_clip=100.,
-# )
 
 path_density = AnnealedDistribution(
     initial_density=initial_density, target_density=target_density, method="geometric"
 )
-
-reference_density = SoftCoreLennardJonesEnergy(
-    dim=39,
-    n_particles=13,
-    sigma=1.0,
-    epsilon_val=1.0,
-    min_dr=1e-3,
-    alpha=0.0,
-    c=0.5,
-    include_harmonic=True,
-)
-key, subkey = jax.random.split(key)
 ts = jnp.linspace(0, 1, 128)
-
-# buffer = SampleBuffer(
-#     buffer_size=25600, min_update_size=1024
-# )
-
-# warmup_counts = 2
-
-# for i in range(warmup_counts):
-#     key, subkey = jax.random.split(key)
-#     samples = generate_samples_with_smc(
-#         key=key,
-#         time_dependent_log_density=path_density.time_dependent_log_prob,
-#         num_samples=5120,
-#         ts=ts,
-#         sample_fn=path_density.sample_initial,
-#         num_steps=10,
-#         integration_steps=20,
-#         eta=0.015,
-#         rejection_sampling=True,
-#         ess_threshold=0.5,
-#         # incremental_log_delta=path_density.incremental_log_delta
-#     )
-
-#     key, subkey = jax.random.split(key)
-#     buffer.add_samples(key, samples["positions"], samples["weights"])
-#     print(f"Warmup {i} done")
-
 print("Warmup done")
 keys = jax.random.split(key, 3)
 key = keys[0]
@@ -94,17 +41,16 @@ covariance_key = keys[2]
 samples = generate_samples_with_smc(
     key=subkey,
     time_dependent_log_density=path_density.time_dependent_log_prob,
-    num_samples=25600,
+    num_samples=10240,
     ts=ts,
     sample_fn=path_density.sample_initial,
-    num_steps=20,
-    integration_steps=50,
-    eta=0.015,
+    num_steps=10,
+    integration_steps=100,
+    eta=0.02,
     rejection_sampling=True,
-    ess_threshold=0.5,
-    # covariances=buffer.estimate_covariance(covariance_key, num_samples=10240),
-    # incremental_log_delta=path_density.incremental_log_delta
+    ess_threshold=0.6,
 )
-fig = reference_density.visualise(samples["positions"][-1])
-# plt.show()
+print("Sampling done")
+print("ESS", samples["ess"])
+fig = target_density.visualise(samples["positions"][-1])
 plt.savefig("lj13c.png")
