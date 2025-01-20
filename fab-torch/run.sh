4b8b1e157930e0c860f51630a7071f1f3256216b
J-zin <670163670@qq.com>
2025-01-20 04:29:05 +0000

    motify run,sh

---
+++ fab-torch/run.sh
@@ -0,0 +1,2 @@
+# python experiments/gmm/run.py training.use_buffer=False training.prioritised_buffer=False
+# python experiments/gmm/run.py training.use_buffer=True training.prioritised_buffer=True
