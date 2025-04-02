98d23e4569526cdab9fe92443f4fbc162094f433
Wccccc <43540548+cjim8889@users.noreply.github.com>
2025-04-02 16:45:50 +0100

    Refactor main function: Update n_particles and n_spatial_dim for mw32 target configuration

--- main.py
+++ main.py
@@ -465,8 +465,8 @@ def main():
         n_spatial_dim = None
     elif args.target == "mw32":
         input_dim = 32
-        n_particles = 16
-        n_spatial_dim = 2
+        n_particles = 32
+        n_spatial_dim = 1
     elif args.target in ["dw4", "dw4o"]:
         input_dim = 8
         n_particles = 4
