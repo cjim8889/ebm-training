4aba77a991c0e1d75047ec3b2e633262adcf9c3d
Wccccc <43540548+cjim8889@users.noreply.github.com>
2025-04-04 17:09:47 +0100

    Update ParticleTransformer to v5 and add new scripts for lj55q sampling and visualization

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -17,3 +17,4 @@ from .transformer_v2 import ParticleTransformerV2
 from .transformer_v3 import ParticleTransformerV3
 from .transformer_v4 import ParticleTransformerV4
 from .transformer_v5 import ParticleTransformerV5
+from .transformer_v6 import ParticleTransformerV6
