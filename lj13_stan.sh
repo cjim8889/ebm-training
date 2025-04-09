286371d96b51e28a9f3f7f3475b45a6a1673c27a
wc5118 <43540548+cjim8889@users.noreply.github.com>
2025-04-09 18:17:08 +0100

    stan data

--- lj13_stan.sh
+++ lj13_stan.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-#PBS -l select=1:ncpus=64:mem=256gb
-#PBS -l walltime=72:00:00
-#PBS -j oe
-#PBS -o lj13_stan_output.log
-#PBS -M wc5118@ic.ac.uk                                               
-#PBS -m bae
-
-cd $PBS_O_WORKDIR
-
-
-eval "$(~/anaconda3/bin/conda shell.bash hook)"
-source activate py12
-
-python -m experiments.sample_lj13_harmonic_stan --max-depth 16 --num-samples 300000 --num-warmup 100000 --num-chains 64 --thinning 10
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:461d489ba9c9c7493bcae35cf860564614043e130a1c5d10191bdbb8feb49b3f
+size 424
