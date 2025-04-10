28d307b64e49ff308a871c4888d0e963e9c3626e
Wccccc <43540548+cjim8889@users.noreply.github.com>
2025-04-10 15:01:48 +0100

    Refactor lj13_job.sh to include job configuration and update Python execution parameters

--- lj13_job.sh
+++ lj13_job.sh
@@ -1,3 +1,53 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a788b47a88bb23979415473831ca19ef0bc5ca961f0de57ff17d7b82962f6dde
-size 1157
+#!/bin/bash
+
+#PBS -l select=1:ncpus=4:mem=64gb:ngpus=1:gpu_type=L40S
+#PBS -l walltime=72:00:00
+#PBS -j oe
+#PBS -o lj13_output.log
+#PBS -M wc5118@ic.ac.uk                                               
+#PBS -m bae
+
+cd $PBS_O_WORKDIR
+
+
+eval "$(~/anaconda3/bin/conda shell.bash hook)"
+source activate py12
+
+python main.py \
+  --num-samples 2560 \
+  --depth 6 \
+  --hidden-dim 128 \
+  --embedding-dim 128 --embedder-width 128 --embedder-depth 3 \
+  --network transformer8 \
+  --num-epochs 20000 \
+  --steps-per-epoch 1000 \
+  --mcmc-method vsmc \
+  --mcmc-step-size 0.01 \
+  --mcmc-steps 12 \
+  --mcmc-integration-steps 10 \
+  --initial-sigma 2.5 \
+  --target smlj13q \
+  --seed 12345 \
+  --training-data combined \
+  --batch-size 128 \
+  --learning-rate 1e-04 \
+  --gradient-norm 1. \
+  --optimizer adamw \
+  --weight-decay 0. \
+  --time-batch-size 4 \
+  --n-samples-eval 5000 \
+  --eval-frequency 10 \
+  --shortcut-weight 0.1 \
+  --include-harmonic \
+  --estimator hutchinson \
+  --n-probes 3 \
+  --r-min 0.8 \
+  --num-heads 4 \
+  --lambda-epochs 500 \
+  --ess-threshold 0.5 \
+  --perturb --perturbation-scale .5 \
+  --log-z-estimation-frequency 1 \
+  --augment --translation-scale 10. --continuous-time \
+  --mixed-precision --use-shortcut --skip-shortcut
+
+
